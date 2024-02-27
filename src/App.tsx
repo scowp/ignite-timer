8964d619da5b964b57779f079778901c60767e8d
scowp <matheusgcampo@gmail.com>
2024-02-26 22:06:46 -0300

    projeto criado e configurações de estilo iniciais

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from 'styled-components'
+import Button from './components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export default function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='success'/>
+      <Button variant='danger'/>
+      <Button/>
+
+      <GlobalStyle/>
+    </ThemeProvider>
+  )
+}
+
+
